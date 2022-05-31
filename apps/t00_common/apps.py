22b2fb5a85caf57067df79fee20ca6d4a9d4cc6f
tts885 <guangdong.chen@outlook.com>
2022-05-31 21:09:55 +0900

    Initialize repository

---
+++ apps/t00_common/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class T00CommonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.t00_common'
---
+++ apps/t02_manage/urls.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  3 18:40:56 2022
+
+@author: guangdong.chen
+"""
+
+from django.urls import path
+
+app_name = 't00_manage'
+
+urlpatterns = [
+
+
+    ]
